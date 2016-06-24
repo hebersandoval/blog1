a0cf61367855175a0b7f6ab287343abbaec6bda4
Heber Sandoval <he3ers@gmail.com>
2016-06-24 15:49:53 -0400

    Post can be updated an redirected to the show page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
+  end
 
   private
 
