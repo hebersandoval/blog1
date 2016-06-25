eae37ccc547401c5b54a8fa327260798c8e78f2c
Heber Sandoval <he3ers@gmail.com>
2016-06-24 21:33:00 -0400

    Create validation on user and post model along with displaying validation errors in views, using partials.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  validates :title, presence: true
+  validates :content, presence: true
+
   def post_summary
     self.title + " - " + self.content
   end
