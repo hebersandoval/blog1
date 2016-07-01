b8f57f4e85e308aa4a47376b3248bc61912c5084
Heber Sandoval <he3ers@gmail.com>
2016-07-01 07:20:34 -0400

    Can create a comment that is associated with a post.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,26 @@
 class CommentsController < ApplicationController
+  # def show
+  #   @comment = Comment.find(params[:id])
+  # end
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment]. permit(:content))
+    @comment = @post.comments.create(comment_params)
 
     redirect_to post_path(@post)
+  end
+
+  # def destroy
+	# 	@post = Post.find(params[:post_id])
+	# 	@comment = @post.comments.find(params[:id])
+	# 	@comment.destroy
+  #
+	# 	redirect_to post_path(@post)
+	# end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
