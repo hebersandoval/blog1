04d6effd9d48ad75bc2f67a26c089229788d52da
Heber Sandoval <he3ers@gmail.com>
2016-07-01 09:43:47 -0400

    Add password_digest to users table and simple password validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password, presence: true
+
+  has_secure_password
 end
