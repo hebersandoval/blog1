d68f1200871d8ae4f5dfef8dccdb50823f8215c8
Heber Sandoval <he3ers@gmail.com>
2016-07-01 14:43:08 -0400

    User can be retrieved on subsequent pages using session[:user_id].

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
     # this places a temporary cookie on the user’s browser containing an encrypted version of the user’s id, which allows us to retrieve the id on subsequent pages using session[:user_id]
     session[:user_id] = user.id
   end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
 end
