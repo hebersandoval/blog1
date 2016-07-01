d02babd4e4670ea6b34d63f1d9dc161595d4e307
Heber Sandoval <he3ers@gmail.com>
2016-07-01 09:24:52 -0400

    Add simple email validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_many :posts, through: :comments
 
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
