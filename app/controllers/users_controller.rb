6bc79b689c463b6a55a9584dc47060834d088609
Heber Sandoval <he3ers@gmail.com>
2016-07-01 10:50:20 -0400

    Add the contents of the 'flash' message in the 'views/layouts/application.html.erb'.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Blog App!"
       redirect_to user_path(@user)
     else
       render "new"
